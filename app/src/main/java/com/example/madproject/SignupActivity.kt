246325dcac8211f22dec501f4f939814199ae8ec
it21250156 <it21250156@my.sliit.lk>
2023-05-02 20:33:29 +0530

    login now, register now (janith)

--- app/src/main/java/com/example/madproject/SignupActivity.kt
+++ app/src/main/java/com/example/madproject/SignupActivity.kt
@@ -1,11 +1,20 @@
 package com.example.madproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class SignupActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+
+        val logintxt: TextView = findViewById(R.id.tv_login_now)
+
+        logintxt.setOnClickListener {
+            val intent = Intent(this, LoginActivity2::class.java)
+            startActivity(intent)
+        }
     }
 }
