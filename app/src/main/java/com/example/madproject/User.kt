c8ee44519bc890eab050c68c2eca100ff8986019
it21250156 <it21250156@my.sliit.lk>
2023-05-04 00:16:22 +0530

    login,register with admin login (janith)

--- app/src/main/java/com/example/madproject/User.kt
+++ app/src/main/java/com/example/madproject/User.kt
@@ -1,6 +1,9 @@
 package com.example.madproject
 
 data class User(
-    val name : String? = null, val email : String? = null, val mobileNo : String? = null, val password : String? = null) {
+    val name: String = "",
+    val email: String = "",
+    val mobileNumber: String = "",
+    val password: String = ""
+)
 
-    }
